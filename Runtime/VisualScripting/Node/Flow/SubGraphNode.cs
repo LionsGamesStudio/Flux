487976b4c511db1ce96114771687a11e22e6b2c4
Florian Maillard <81640753+eraflo@users.noreply.github.com>
2025-10-13 22:05:51 +0200

    fix: pipeline static dependencies

--- Runtime/VisualScripting/Node/Flow/SubGraphNode.cs
+++ Runtime/VisualScripting/Node/Flow/SubGraphNode.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using System.Collections.Generic;
 using FluxFramework.Attributes.VisualScripting;
 
 namespace FluxFramework.VisualScripting.Node
